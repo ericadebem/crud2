cfe5a4b6d6784df4aadd9492b871e445768fbcc2
ERICA DE BEM <110907550+ericadebem@users.noreply.github.com>
2022-11-20 14:58:49 +0100

    bug edit

--- assets/script.js
+++ assets/script.js
@@ -85,3 +85,9 @@ cancelEditBtn.addEventListener("click", (e) => {
 
     toggleForms();
 });
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const editInputValue = editInput.value
+    
+}
